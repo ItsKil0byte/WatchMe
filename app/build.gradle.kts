240de546d928580e7cacdb304e640fe11638ed05
kilobyte <itskilobyte@gmail.com>
2026-01-10 16:19:27 +0500

    add serialization dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
 }
 
@@ -77,6 +78,9 @@ dependencies {
 
     // Coil
     implementation(libs.coil.compose)
+
+    // Serialization
+    implementation(libs.serialization)
 
     // Testing
     testImplementation(libs.junit)
