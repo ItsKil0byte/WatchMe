4e7a0a9c1e43eba80309260e1814ebc99818cfe7
Alexander Lebedev <itskilobyte@gmail.com>
2026-01-12 17:37:01 +0500

    add a base UI searh bar in movie list screen

--- app/src/main/java/ru/ae/watchme/ui/screens/MovieListScreen.kt
+++ app/src/main/java/ru/ae/watchme/ui/screens/MovieListScreen.kt
@@ -2,18 +2,27 @@ package ru.ae.watchme.ui.screens
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,6 +46,9 @@ fun MovieListScreen(
         )
     }
 
+    var searchQuery by remember { mutableStateOf("") }
+    var isActive by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -52,6 +64,27 @@ fun MovieListScreen(
             )
         }
     ) { innerPadding ->
+
+        SearchBar(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            query = searchQuery,
+            onQueryChange = {searchQuery = it},
+            onSearch = {isActive = false},
+            active = isActive,
+            onActiveChange = {isActive = it},
+            placeholder = {Text("Найти фильм...")},
+            leadingIcon = { Icon(Icons.Default.Search, contentDescription = null)},
+            trailingIcon = {
+                if (isActive) {
+                    IconButton(onClick = {searchQuery = ""}) {
+                        Icon(Icons.Default.Clear, contentDescription = null)
+                    }
+                }
+            }
+        ) {
+            // TODO: Полезное действие...
+        }
+
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
